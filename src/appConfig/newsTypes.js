e9622c33f1c9368c5306b685f6c90ea64f9e05e6
李朋 <958578466qq.com>
2017-08-05 22:35:33 +0800

    修改路由

---
+++ src/appConfig/newsTypes.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    type: 0,
+    title: '信工新闻眼'
+  },
+  {
+    type: 1,
+    title: '特色活动'
+  },
+  {
+    type: 2,
+    title: '通知早知道'
+  },
+  {
+    type: 3,
+    title: '党建一点通'
+  },
+  {
+    type: 4,
+    title: '制度建设'
+  },
+  {
+    type: 5,
+    title: '制度建设'
+  },
+  {
+    type: 6,
+    title: '随时随地学'
+  },
+  {
+    type: 7,
+    title: '随时随地拍'
+  },
+  {
+    type: 8,
+    title: '政治学习'
+  }
+]
