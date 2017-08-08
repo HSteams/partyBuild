b4c3254fcb1bc62b22b8dfcf87347a81c6f8ecc2
李文林 <1972403767@qq.com>
2017-08-08 11:20:17 +0800

    图片

--- src/components/forum/forumSave.vue
+++ src/components/forum/forumSave.vue
@@ -1,21 +1,30 @@
 <template>
-<div>
-   <span>标题：</span>
-  <input type="text" placeholder="请输入标题">
-  <textarea  cols="30" rows="10" placeholder="我要发帖">
-  </textarea>
-  <button class="btn">发表</button>
-</div>
+  <div>
+    <span>标题：</span>
+    <input type="text" placeholder="请输入标题">
+    <textarea cols="30" rows="10" placeholder="我要发帖">
+   </textarea>
+    <button class="btn">发表</button>
+  </div>
 </template>
 <script>
-
+  export default {
+    data () {
+      return {
+        header: {
+          title: '我要发帖'
+        }
+      }
+    }
+  }
 </script>
 <style lang="less">
-  div{
+  div {
     text-align: center;
   }
-  .btn{
-    width:120px;
+
+  .btn {
+    width: 120px;
     color: #b6000a;
   }
 
